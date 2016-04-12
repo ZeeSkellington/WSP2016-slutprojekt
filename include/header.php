2a2c74877eb90520fbd1ec0431e82b90bd143b32
ZeeSkellington <Chickenripper121@gmail.com>
2016-04-12 12:11:36 +0200

    We require more battle medics.

---
+++ include/header.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: adam almqvist
+ * Date: 2016-04-08
+ * Time: 12:15
+ */
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>FiGybook</title>
+</head>
+<body>
+
--- index.php
+++ index.php
@@ -5,3 +5,22 @@
  * Date: 2016-04-05
  * Time: 12:56
  */
+	$page="";
+//checks if GET exists
+if (isset($_GET["page"]))
+{
+    $page=$_GET["page"];
+}
+	//require our header, sidebar etc
+	require "include/header.php";
+	// Deal with the different page contents
+	if (file_exists("content/{$page}.php"))
+    {
+        //require "content/{$page}.php";
+    }
+    else
+    {
+        require "";
+    }
+	require "";
+?>
