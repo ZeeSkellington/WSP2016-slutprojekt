3098f1b6ac6fd2b153231ee2bc4f17878dbe7470
ZeeSkellington <Chickenripper121@gmail.com>
2016-04-12 12:15:22 +0200

    Sure sure. Keep your shirt on.

--- include/header.php
+++ include/header.php
@@ -4,12 +4,14 @@
  * User: adam almqvist
  * Date: 2016-04-08
  * Time: 12:15
+ * sure, sure. Keep your shirt on.
  */
 ?>
 <!DOCTYPE html>
 <html>
 <head>
     <title>FiGybook</title>
+
 </head>
 <body>
 
